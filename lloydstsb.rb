e6213012fcf2210a205dfd981d762b4558361d6d
Martin Paul Eve <martin@martineve.com>
2011-12-30 18:44:03 +0000

    Add email address

--- lloydstsb.rb
+++ lloydstsb.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 =begin
-    Copyright 2011 Martin Paul Eve
+    Copyright 2011 Martin Paul Eve (martin@martineve.com)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
